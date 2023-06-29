01aee3e2bec3933ba5f318f06c29a749543f36fb
akashi7 <christiannseko@gmail.com>
2023-06-29 17:04:26 +0200

    FIXED lengthv of signup button

--- src/components/auth/signup.jsx
+++ src/components/auth/signup.jsx
@@ -118,7 +118,7 @@ const SignUp = () => {
               {!loader && (
                 <button
                   type='submit'
-                  className='w-full h-[50px] bg-blue text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                  className='w-[50%] h-[50px] bg-blue text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                 >
                   SIGN UP
                 </button>
